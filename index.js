3dbd19c6d532531738d11ece17d63ab96586bf51
notoiro297 <notoiro297@gmail.com>
2025-05-02 00:37:19 +0900

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const {
+  TOKEN, SERVER_ID, CHANNEL_ID, MENTION, TARGET_IDS
+} = require('./config.json');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildPresences
+  ]
+});
+
+client.once('ready', () => {
+  console.log("Bot is online!");
+});
+
+client.on('presenceUpdate', (_, newPresence) => {
+  if(TARGET_IDS.some(id => newPresence.userId === id)) {
+    const channel = client.channels.cache.get(CHANNEL_ID);
+    if(newPresence.status === "offline"){
+      if (channel) channel.send(`⚠️ <@&${MENTION}> <@${newPresence.userId}> がオフラインになりました！`);
+    }else if(newPresence.status === "online"){
+      if (channel) channel.send(`✅️ <@&${MENTION}> <@${newPresence.userId}> がオンラインになりました！`);
+
+    }
+
+
+  }
+});
+
+client.login(TOKEN);
